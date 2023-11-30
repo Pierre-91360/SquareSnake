ebf60a36d657166cf08c7f47bf72ba13f9080c40
Pierre-91360 <pierre97.schiltz91@gmail.com>
2023-11-30 19:13:17 +0100

    small correction on the compiler file

--- compile_main.sh
+++ compile_main.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 proj_path=$(dirname $(readlink -f "$0"))/
+if [ ! -d "$proj_path"/out ]; then
+    mkdir "$proj_path"/out
+fi
 g++ "$proj_path"src/*.cpp -o "$proj_path"out/game $(sdl2-config --cflags --libs)
