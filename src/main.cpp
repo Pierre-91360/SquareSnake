9fcf455c0f21463b5ffd80098e4af2d9d6a2e6cd
Pierre-91360 <pierre97.schiltz91@gmail.com>
2023-12-04 10:28:53 +0100

    useless lines removed in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,6 @@
 
 int main(int argc, char *argv[])
 {
-    SDL_version version;
-    SDL_VERSION(&version);
     srand((unsigned int)time(NULL));
     SDL_Initialisation();
     Game *game = new Game();
